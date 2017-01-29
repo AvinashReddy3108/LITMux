da63384b6ff3ae78b8221a82770d149b7a3358ec
cabbage <root@everdream.xyz>
2017-01-30 00:02:31 +0800

    install.sh updated.

--- install.sh
+++ install.sh
@@ -3,19 +3,18 @@ termux-setup-storage
 
 apt update
 apt install -y git zsh
-git clone https://github.com/Cabbagec/termux-ohmyzsh.git $HOME/termux-ohmyzsh
+git clone https://github.com/Cabbagec/termux-ohmyzsh.git $HOME/termux-ohmyzsh --depth 1
 
-mv $HOME/.termux $HOME/.termux.bak
+rm -fr $HOME/.termux/*
+cp -R $HOME/termux-ohmyzsh/.termux/* $HOME/.termux/
 
-mv $HOME/termux-ohmyzsh/.termux $HOME/.termux
-
-git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 sed -i '/^ZSH_THEME/d' $HOME/.zshrc
 sed -i '1iZSH_THEME="agnoster"' $HOME/.zshrc
 chsh -s zsh
 
-echo "oh-my-zsh install complete!\nChoose your color theme now~"
+echo "oh-my-zsh install complete!\nChoose your color scheme now~"
 $HOME/.termux/colors.sh
 
 echo "Please restart Termux app..."
