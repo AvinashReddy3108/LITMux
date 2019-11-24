ce9340e9ad539a2381de2d9dbfabc4106a47211b
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-11-24 15:09:13 +0530

    Plugin handler - dedupes the current list in .zshrc

--- install.sh
+++ install.sh
@@ -22,6 +22,8 @@ sed_handle_plugin_zshrc () {
    true
    else
    sed -i "s/\(^plugins=([^)]*\)/\1 $1/" ~/.zshrc
+   PLUGINS_DEDUPED = $plugins | sed -e 's/\b\([a-z]\+\)[ ,\n]\1/\1/g'
+   sed -i "s/\(^plugins=([^)]*\)/$PLUGINS_DEDUPED/" ~/.zshrc
    fi
 }
 
