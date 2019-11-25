50b42b3409559722465ba2c7dc4ceaa8c75aa84e
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-11-25 20:56:26 +0530

    Update install.sh
    - Finally, got a solution to the zshrc plugin handler.
    - Beautify the install script with ASCII art.

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
+
+show_banner() {
 clear
 echo "██╗     ██╗████████╗███╗   ███╗██╗   ██╗██╗  ██╗";
 echo "██║     ██║╚══██╔══╝████╗ ████║██║   ██║╚██╗██╔╝";
@@ -8,7 +10,9 @@ echo "███████╗██║   ██║   ██║ ╚═╝ ██
 echo "╚══════╝╚═╝   ╚═╝   ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝";
 echo "";
 echo "      Ditch the boring BASH and get LIT !!      ";
-sleep 3
+echo ""
+echo ""
+}
 
 git_force_clone_shallow () {
    if [ -d "$2" ]; then
@@ -18,8 +22,8 @@ git_force_clone_shallow () {
 }
 
 sed_handle_plugin_zshrc () {
-   if grep -q "$1" "$plugins" ; then
-   echo "The ZSH plugin '$1' is already installed in the .zshrc file. Skipping.."
+   if grep "plugins=" ~/.zshrc | sed -n 2p | grep "$1" ; then
+   echo "The ZSH plugin '$1' is already installed in the .zshrc file."
    else
    sed -i "s/\(^plugins=([^)]*\)/\1 $1/" ~/.zshrc
    fi
@@ -34,14 +38,23 @@ sed_handle_alias_zshrc () {
    fi
 }
 
+# Welcome.
+show_banner
+sleep 3
+
 # Giving Storage permision to Termux App.
 termux-setup-storage
 
 # Updating package repositories and installing requirements.
-pkg update
+echo "Checking repositories for updated packages, please wait..."
+pkg update > /dev/null 2>> ~/litmux_err.log
+
+show_banner
+echo "Installing required packages, please wait...."
 pkg install -y git zsh
 
 # Installing Oh My ZSH as a replacement of BASH.
+show_banner
 echo "Installing Oh-My-ZSH..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended" > /dev/null 2>> ~/litmux_err.log
 
@@ -55,11 +68,13 @@ sed_handle_alias_zshrc "litmux-upgrade" "'$HOME/.oh-my-zsh/custom/misc/LitMux/up
 sed_handle_alias_zshrc "litmux-purge" "'$HOME/.oh-my-zsh/custom/misc/LitMux/uninstall.sh'"
 
 # Installing "Syntax Highlighting" addon for ZSH, and appending that to the plugins list.
+show_banner
 echo "Installing 'Syntax Highlighting' addon for Oh-My-ZSH..."
 git_force_clone_shallow https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" > /dev/null 2>> ~/litmux_err.log
 sed_handle_plugin_zshrc "zsh-syntax-highlighting"
 
 # Installing "Custom Plugins Updater" addon for ZSH, and appending that to the plugins list.
+show_banner
 echo "Installing 'Custom Plugins Updater' addon for Oh-My-ZSH..."
 git_force_clone_shallow https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "$HOME/.oh-my-zsh/custom/plugins/autoupdate" > /dev/null 2>> ~/litmux_err.log
 sed_handle_plugin_zshrc "autoupdate"
@@ -68,16 +83,19 @@ sed_handle_plugin_zshrc "autoupdate"
 git_force_clone_shallow https://github.com/AvinashReddy3108/LitMux.git "$HOME/.oh-my-zsh/custom/misc/LitMux" > /dev/null 2>> ~/litmux_err.log
 
 # Installing powerlevel10k theme for ZSH, and making it the current theme in .zshrc file.
+show_banner
 echo "Installing 'Powerlevel10K' theme for ZSH..."
 git_force_clone_shallow https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" > /dev/null 2>> ~/litmux_err.log
 sed -i 's~\(ZSH_THEME="\)[^"]*\(".*\)~\1powerlevel10k/powerlevel10k\2~' ~/.zshrc
 
 # Installing the Powerline font for Termux.
+show_banner
 echo "Installing the Powerline patched font for Termux..."
 curl -fsSL -o ~/.termux/font.ttf 'https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf' > /dev/null 2>> ~/litmux_err.log
 
 # Set 'Tango' as the default color scheme for the shell.
 if [ ! -e ~/.termux/colors.properties ]; then
+show_banner
 echo "Changing default color scheme for Termux..."
 cp -fr "$HOME/.oh-my-zsh/custom/misc/LitMux/.termux/colors.properties" ~/.termux/colors.properties
 termux-reload-settings
@@ -99,9 +117,15 @@ echo "      Installation Complete, gimme cookies.     ";
 sleep 3
 
 if [ ! -f ~/.p10k.zsh ]; then
-    clear
-    echo "Initialising the configuration wizard for Powerlevel10k theme, please wait."
-    sleep 2
-    exec zsh -l
+    if [ ! grep -q "p10k.zsh" ~/.zshrc ]; then
+      clear
+      show_banner
+      echo "Initialising the configuration wizard for Powerlevel10k theme, please wait."
+      sleep 2
+      exec zsh -l
+    fi
 fi
-exit
+
+if [ "$0" = "bash" ]; then
+exec zsh -l
+done
