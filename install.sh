e6366c87508385b3760cbe1cf91ee34e8fe2f3ec
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2020-12-19 04:52:26 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,13 +30,6 @@ echo -n -e "Syncing with fastest mirrors. \033[0K\r"
 done
 sleep 2
 
-# Update package lists.
-echo -n -e "Updating package lists. \033[0K\r"
-(echo 'n' | apt update) | while read -r line; do
-    :
-done
-sleep 2
-
 # Upgrade packages.
 echo -n -e "Upgrading packages. \033[0K\r"
 shutt apt-get upgrade -o Dpkg::Options::='--force-confnew' -y
@@ -44,7 +37,7 @@ sleep 2
 
 # Updating package repositories and installing packages.
 echo -n -e "Installing required packages. \033[0K\r"
-shutt apt install -y curl git zsh man
+shutt apt update && apt install -y curl git zsh man
 sleep 2
 
 # Installing SUDO.
